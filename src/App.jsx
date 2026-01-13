2e7df960961ef144204bb496ba20e980f0bdf485
Kadi Uday <udaykadi45@gmail.com>
2026-01-13 16:06:50 +0530

    added routing by router dom and created navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-function App() {
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import Body from "./Body";
 
+function App() {
   return (
     <>
-      
-      <h1>Hello World</h1>
-      
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />} >
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
